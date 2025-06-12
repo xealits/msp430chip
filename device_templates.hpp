d86d0c1dd7297c8798011d8c6689cbe03e253311
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2025-06-13 00:06:27 +0200

    Trying out a struct per register
    use as:
    
    {
      using ctr = device::TimerA_0::Control;
      ctr::write<
        ctr::input_clock::set(ctr::INPUT_CLOCK_ACLK) |
        ctr::clock_divider::set(ctr::DIVIDE_0) |
        ctr::mode_control::set(ctr::MODE_CONT)
    }
    
    And there should be a typed way to set the bit fields.
    
    	modified:   device_templates.hpp

--- device_templates.hpp
+++ device_templates.hpp
@@ -2,30 +2,51 @@
 #include <type_traits>
 
 #include "bitlogic.hpp"
+#include "msp430g2553.h"
 
 namespace devices {
 template <volatile unsigned int& reg_CTL, volatile unsigned int& reg_CCTL>
 struct TimerA {
   TimerA() = delete;
-  using reg_CTL_type = std::decay_t<decltype(reg_CTL)>;
-  using input_clock = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 8, 3>;
-  constexpr static typename input_clock::BaseRegT INPUT_CLOCK_TACLK = 0,
-                                                  INPUT_CLOCK_ACLK = 1,
-                                                  INPUT_CLOCK_SMCLK = 2,
-                                                  INPUT_CLOCK_INCLK = 3;
 
-  using clock_divider = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 6, 2>;
-  constexpr static typename clock_divider::BaseRegT DIVIDE_0 = 0, DIVIDE_2 = 1,
-                                                    DIVIDE_4 = 2, DIVIDE_8 = 3;
+  struct Control {
+    using reg_t = std::decay_t<decltype(reg_CTL)>;
 
-  using mode_select = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 4, 2>;
-  constexpr static typename mode_select::BaseRegT MODE_STOP = 0, MODE_UPTO = 1,
-                                                  MODE_CONT = 2, MODE_UPDN = 3;
+    using unused11 = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 11, 5>;
+    using input_clock = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 8, 3>;
 
-  template <reg_CTL_type new_val>
-  constexpr static inline void control_write(void) {
-    reg_CTL = new_val;
-  }
+    using clock_divider = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 6, 2>;
+    using mode_control = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 4, 2>;
+
+    using unused3 = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 3, 1>;
+    using clear = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 2, 1>;
+    using taie = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 1, 1>;
+    using taifg = bitlogic::BitField<decltype(reg_CTL), reg_CTL, 0, 1>;
+
+    constexpr static typename input_clock::BaseRegT INPUT_CLOCK_TACLK = 0,
+                                                    INPUT_CLOCK_ACLK = 1,
+                                                    INPUT_CLOCK_SMCLK = 2,
+                                                    INPUT_CLOCK_INCLK = 3;
+
+    constexpr static typename clock_divider::BaseRegT DIVIDE_0 = 0, DIVIDE_2 = 1,
+                                                      DIVIDE_4 = 2, DIVIDE_8 = 3;
+
+    constexpr static typename mode_control::BaseRegT MODE_STOP = 0, MODE_UPTO = 1,
+                                                     MODE_CONT = 2, MODE_UPDN = 3;
+
+    template <reg_t new_val>
+    constexpr static inline void write(void) {
+      reg_CTL = new_val;
+    }
+
+    constexpr static inline void write(reg_t new_val) {
+      reg_CTL = new_val;
+    }
+
+    constexpr static inline reg_t read(void) {
+      return reg_CTL;
+    }
+  };
 
   using CCTL_interrupt_enable =
       bitlogic::BitField<decltype(reg_CCTL), reg_CCTL, 4, 1>;
