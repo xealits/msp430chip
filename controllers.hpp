f160cd6f447bbfd41ecbfd10a1d76419dc2def9d
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2026-02-06 23:49:40 +0100

    SFRs in MSP430G2553
    Not sure if SFRs can be a generic template for many MSP430 models.
    I think it is likely.
    
    	modified:   controllers.hpp

--- controllers.hpp
+++ controllers.hpp
@@ -2,33 +2,70 @@
 #include <msp430.h>
 
 #include "device_templates.hpp"
+#include "msp430chip/bitlogic.hpp"
 #include "msp430g2553.h"
 
 namespace controllers {
-namespace MSP430G2553 {
-#if defined(__MSP430G2553__)
-
 using bitlogic::DevPack;
+using bitlogic::Register;
+using bitlogic::BitField;
 using regmaps::CaptureCompareBlockTemplate;
 using regmaps::CaptureCompareBlockParams;
 
-using TimerA_0 = regmaps::TimerA<
-    TA0CTL, TA0R, TA0IV,
-    DevPack<CaptureCompareBlockTemplate
-        , CaptureCompareBlockParams<TA0CCTL0, TA0CCR0>
-        , CaptureCompareBlockParams<TA0CCTL1, TA0CCR1>
-        , CaptureCompareBlockParams<TA0CCTL2, TA0CCR2>
-    >>;
+#if defined(__MSP430G2553__)
+namespace MSP430G2553 {
 
-using Port1 = regmaps::PortIO8bitI<P1IN, P1OUT, P1DIR,
-                                   P1SEL, P1SEL2, P1REN,
-                                   P1IFG, P1IES, P1IE>;
-//using Port2 = devices::Port8bitI<P2IN, P2OUT, P2DIR, P2IFG, P2IES, P2IE, P2SEL,
-//                                 P2SEL2, P2REN>;
-//using Port3 = devices::Port8bit<P3IN, P3OUT, P3DIR, P3SEL, P3SEL2, P3REN>;
+    using TimerA_0 = regmaps::TimerA<
+        TA0CTL, TA0R, TA0IV,
+        DevPack<CaptureCompareBlockTemplate
+            , CaptureCompareBlockParams<TA0CCTL0, TA0CCR0>
+            , CaptureCompareBlockParams<TA0CCTL1, TA0CCR1>
+            , CaptureCompareBlockParams<TA0CCTL2, TA0CCR2>
+        >>;
 
-#endif
+    using Port1 = regmaps::PortIO8bitI<P1IN, P1OUT, P1DIR,
+                                    P1SEL, P1SEL2, P1REN,
+                                    P1IFG, P1IES, P1IE>;
+    using Port2 = regmaps::PortIO8bitI<P2IN, P2OUT, P2DIR,
+                                    P2SEL, P2SEL2, P2REN,
+                                    P2IFG, P2IES, P2IE>;
+    using Port3 = regmaps::PortIO8bit<P3IN, P3OUT, P3DIR,
+                                    P3SEL, P3SEL2, P3REN>;
+
+    /// Special Function Registers
+    namespace SFRs {
+        struct InterruptEnable1 : public Register<decltype(IE1), IE1> {
+            using Watchdog = BitField<decltype(IE1), IE1, 0, 1>;
+            using OscFault = BitField<decltype(IE1), IE1, 1, 1>;
+            using NMI = BitField<decltype(IE1), IE1, 4, 1>;
+            using FlashAccessViolation = BitField<decltype(IE1), IE1, 5, 1>;
+        };
+
+        struct InterruptFlag1 : public Register<decltype(IFG1), IFG1> {
+            using Watchdog = BitField<decltype(IFG1), IFG1, 0, 1>;
+            using OscFault = BitField<decltype(IFG1), IFG1, 1, 1>;
+            using PowerOn = BitField<decltype(IFG1), IFG1, 2, 1>;
+            using Reset = BitField<decltype(IFG1), IFG1, 3, 1>;
+            using NMI = BitField<decltype(IFG1), IFG1, 4, 1>;
+        };
+
+        struct InterruptEnable2 : public Register<decltype(IE2), IE2> {
+            using UCA0RX = BitField<decltype(IE2), IE2, 0, 1>;
+            using UCA0TX = BitField<decltype(IE2), IE2, 1, 1>;
+            using UCB0RX = BitField<decltype(IE2), IE2, 2, 1>;
+            using UCB0TX = BitField<decltype(IE2), IE2, 3, 1>;
+        };
+
+        struct InterruptFlag2 : public Register<decltype(IFG2), IFG2> {
+            using UCA0RX = BitField<decltype(IFG2), IFG2, 0, 1>;
+            using UCA0TX = BitField<decltype(IFG2), IFG2, 1, 1>;
+            using UCB0RX = BitField<decltype(IFG2), IFG2, 2, 1>;
+            using UCB0TX = BitField<decltype(IFG2), IFG2, 3, 1>;
+        };
+    };
+
 };  // namespace MSP430G2553
+#endif
 };  // namespace controllers
 
 /*! \brief Launchpad development boards
