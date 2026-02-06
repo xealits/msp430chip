f6ac45890d34802a9c7eaca0876eb0fe97b5dd8c
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2026-02-06 01:04:41 +0100

    And some more structure plus dumping Cpp
    	modified:   generate_headers.py

--- generate_headers.py
+++ generate_headers.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from pathlib import Path
 from sys import argv
 from pprint import pprint
+from dataclasses import dataclass, field
 
 fname = Path(argv[1])
 
@@ -27,22 +28,86 @@ def find_topmost(soup, tag, class_ = None):
 
     return found_els
 
+reg_field_template = "using {name} = BitField<decltype({reg_ref}), {reg_ref}, {offset}, {width}>;"
+reg_template = "struct {name} : public Register<decltype({reg_ref}), {reg_ref}> {body};"
+device_template = """
+template<{reg_declarations}>
+struct {name} {{
+  {name}() = delete;
+
+  {reg_definitions}
+}};
+"""
+
+@dataclass
+class RegField:
+    name: str
+    offset: int
+    width: int
+
+    def to_cpp(self, reg_ref):
+        return reg_field_template.format(name=self.name, reg_ref=reg_ref, offset=self.offset, width=self.width)
+
+@dataclass
+class Register:
+    name: str
+    width: int = 32
+    fields: dict = field(default_factory=dict)
+
+    def __getattr__(self, name):
+        known_types = {32: "uint32_t"}
+        if name == "cpp_type":
+            return known_types.get(self.width)
+
+        raise AttributeError(f"'Register' object has no attribute {name}")
+
+    def to_cpp(self, reg_ref):
+        fields_str = '\n'.join("  " + f.to_cpp(reg_ref) for f in self.fields.values())
+        if fields_str:
+            body = f"{{\n{fields_str}\n}}"
+        else:
+            body = "{}"
+        return reg_template.format(name=self.name, reg_ref=reg_ref, body=body)
+
+# TODO: there are 2 types of devices: unique and templates
+@dataclass
+class Device:
+    name: str
+    registers: dict = field(default_factory=dict)
+
+    def to_cpp(self):
+        reg_decls = []
+        reg_defs = []
+        for reg in self.registers.values():
+            reg_ref = reg.name + "_t"
+            cpp_type = reg.cpp_type
+            reg_decl = f"volatile {cpp_type}& {reg_ref}"
+
+            reg_decls.append(reg_decl)
+            reg_defs.append(reg.to_cpp(reg_ref))
+
+        reg_decls = "\n".join(reg_decls)
+        reg_defs = "\n".join(reg_defs)
+
+        return device_template.format(name=self.name, reg_declarations=reg_decls, reg_definitions=reg_defs)
+
 def parse_field(bs_elem):
     name = bs_elem.select_one(":scope dfn").text.strip()
     offset = int(bs_elem.select_one(":scope span.offset").text.strip())
     width = int(bs_elem.select_one(":scope span.width").text.strip())
-    info = {"offset": offset, "width": width}
-    return name, info
+    #info = {"offset": offset, "width": width}
+    field = RegField(name, offset, width)
+    return name, field
 
 def parse_register(bs_elem):
     name = bs_elem.select_one(":scope dfn:not(:scope details dfn)").text.strip()
     width = 32
-    cpp_type = {32: "uint32_t"}[width]
-    reg_info = {"width": width, "cpp_type": cpp_type}
+    #cpp_type = {32: "uint32_t"}[width]
+    #reg_info = {"width": width, "cpp_type": cpp_type}
 
     # reg fields
     reg_fields = {}
-    reg_info["fields"] = reg_fields
+    reg = Register(name, width, reg_fields)
 
     substr = bs_elem.select_one(":scope details")
     bs_fields = []
@@ -54,22 +119,21 @@ def parse_register(bs_elem):
         assert field_name not in reg_fields
         reg_fields[field_name] = field_info
 
-    return name, reg_info
+    return name, reg
 
 def parse_device_template(bs_elem, known_templates={}):
     dev_name = bs_elem.select_one(":scope dfn:not(:scope details dfn)").text.strip()
     if dev_name in device_templates:
         raise Exception(f"Device template {dev_name} already exists")
 
-    templ = {}
+    dev_registers = {}
+    templ = Device(dev_name, dev_registers)
     known_templates[dev_name] = templ
 
     substr = bs_elem.select_one(":scope details")
 
     # parse registers
     regs = substr.select(":scope li.register:not(:scope details li.register)")
-    dev_registers = {}
-    templ["registers"] = dev_registers
     for reg in regs:
         name, info = parse_register(reg)
         assert name not in dev_registers
@@ -93,3 +157,9 @@ for dev in devices:
     parse_device_template(dev, device_templates)
 
 pprint(device_templates)
+
+for reg in device_templates["TimerA"].registers.values():
+    reg_ref = reg.name + "_t"
+    print(reg.to_cpp(reg_ref))
+
+print(device_templates["TimerA"].to_cpp())
